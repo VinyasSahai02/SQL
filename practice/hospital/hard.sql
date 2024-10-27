09cddccaa503ec5384a4a5cb876cc3f4d5f56901
vinyas <vinyassahai03@gmail.com>
2024-10-27 20:32:47 +0530

    completed hard questions

--- practice/hospital/hard.sql
+++ practice/hospital/hard.sql
@@ -87,3 +87,69 @@ select province_name from patients
 join province_names on province_names.province_id = patients.province_id
 group by province_name
 having sum(case when gender= 'M' then 1 else 0 end) > sum(case when gender= 'F' then 1 else 0 end)
+
+-- We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+-- - First_name contains an 'r' after the first two letters.
+-- - Identifies their gender as 'F'
+-- - Born in February, May, or December
+-- - Their weight would be between 60kg and 80kg
+-- - Their patient_id is an odd number
+-- - They are from the city 'Kingston'
+select * from patients
+where first_name like "__r%" and
+gender ='F' and
+weight between 60 and 80 and
+patient_id%2!=0 and
+city like "Kingston"
+
+-- Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
+SELECT 
+    concat(ROUND((SUM(CASE WHEN gender = 'M' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)),2),'%')
+FROM patients;
+
+-- For each day display the total amount of admissions on that day. Display the amount changed from the previous date.
+select admission_date,count(*), count(*)-LAG(COUNT(*)) OVER (ORDER BY admission_date) from admissions
+group by admission_date
+order by admission_date
+
+-- Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+select province_name
+from province_names
+order by
+  (not province_name = 'Ontario'),
+  province_name
+-- or 
+select province_name
+from province_names
+order by
+  province_name = 'Ontario' desc,
+  province_name
+-- or 
+SELECT province_name
+FROM province_names
+ORDER BY
+  CASE
+    WHEN province_name = 'Ontario' THEN 1
+    ELSE province_name
+  END
+-- or 
+select province_name
+from province_names
+order by
+  (case when province_name = 'Ontario' then 0 else 1 end),
+  province_name
+
+-- We need a breakdown for the total amount of admissions each doctor has started each year.
+-- Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+SELECT
+  doctors.doctor_id as doctor_id,
+  CONCAT(doctors.first_name,' ', doctors.last_name) as doctor_name,
+  doctors.specialty,
+  YEAR(admissions.admission_date) as selected_year,
+  COUNT(*) as total_admissions
+FROM doctors
+  LEFT JOIN admissions ON doctors.doctor_id = admissions.attending_doctor_id
+GROUP BY
+  doctor_name,
+  selected_year
+ORDER BY doctor_id, selected_year
